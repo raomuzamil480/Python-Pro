cfe18be3e525ef9c1251ce4d11c3a0a22a59a4ad
raomuzamil480 <raomuzamil480@gmail.com>
2026-07-01 19:55:33 +0500

    List_Toupls_sets

---
+++ PROJECTS/Arrayeas.py
@@ -0,0 +1,27 @@
+#List = [] ordered and changeable and dublicate ok
+#set ={} add/remove but no dublicate
+# Tuple =()  duplicate ok and changeable
+#======================
+#List
+
+a=('apple','g','data','dates')
+
+# a.append('banana')
+# a.sort()
+# a.pop(2)
+# a.reverse()
+# print(a.index('g'))
+# print(a)
+# print(dir(a))
+
+#sets
+# a.add('jsj')
+# a.pop()
+# a.remove('g')
+# print(a)
+
+#Touples
+
+# print(a.index('apple'))
+# print(a.count('apple'))
+# print(a)
