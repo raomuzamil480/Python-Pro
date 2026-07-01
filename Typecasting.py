4872c1e917b59e392aa67c61f8e494566127a218
raomuzamil480 <raomuzamil480@gmail.com>
2026-07-01 12:29:50 +0500

    TypeCasting

---
+++ Typecasting.py
@@ -0,0 +1,17 @@
+# The Typecasting the process of converting the value of one data thpe into another fata type
+
+# int() , str() ,float(),bool()
+
+name = "John"
+age = 25
+height = 5.9
+is_student = True
+
+# Typecasting
+age_str = str(age)  # Convert int to str
+height_int = int(height)  # Convert float to int
+is_student_str = str(is_student)  # Convert bool to str
+
+print(age_str)
+print(height_int)
+print(is_student_str)
