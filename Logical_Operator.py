66cc7f4a42925b55151dd559fc1830d22b5bc33f
raomuzamil480 <raomuzamil480@gmail.com>
2026-07-01 15:02:01 +0500

    Logical

---
+++ Logical_Operator.py
@@ -0,0 +1,13 @@
+# Logical Operator =
+        # or
+        # and
+        # not
+
+tem = 334
+is_suny = False
+
+if tem > 45 and   is_suny:
+    print("not valid")
+
+elif tem > 30 or is_suny:
+    print("very hot")
