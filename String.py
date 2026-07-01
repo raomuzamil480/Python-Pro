59cc1a0df73345d3e410671c0f37238b168260fa
raomuzamil480 <raomuzamil480@gmail.com>
2026-07-01 15:25:31 +0500

    Stirng

--- String.py
+++ String.py
@@ -1,10 +1,14 @@
 # String all methods
 # |
 # |
-# print(help(str))
+#================= print(help(str)) ==========================
 
 name = input('Please Enter Name :')
 
 # name = name.rfind('W')
 name = name.isdigit()
 print(name)
+
+#=============indexing  [start:end:step] =================
+# number=123-432-32323
+# print(number[3:4:5])
