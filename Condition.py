1b047c135105b11e76a7389207daa42b6b14e6e1
raomuzamil480 <raomuzamil480@gmail.com>
2026-07-01 13:32:56 +0500

    Conditions

--- Condition.py
+++ Condition.py
@@ -1,1 +1,13 @@
-#   
+#   If Else
+try:
+    age = int(input('Please Enter your Age :'))
+
+    if age >= 18:
+     print('YOU ARE ADULT ')
+
+    elif age >=0:
+        print('YOU ARE NOT ADULT')
+    else:
+        print('PLEASE ENTER CORRECT AGE')
+except ValueError:
+    print("Please enter an integer (number only)")
