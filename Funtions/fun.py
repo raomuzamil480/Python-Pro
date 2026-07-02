618022fdac91956edd2c61e8d06a1f950a36a23e
raomuzamil480 <raomuzamil480@gmail.com>
2026-07-02 12:38:42 +0500

    Funtion_return

---
+++ Funtions/fun.py
@@ -0,0 +1,11 @@
+#FUnction are Block of Code
+
+def Blue(name,age):
+    print(f"Name is {name} and age is {age}")
+Blue('rao',23)
+
+# Return Keyword
+def red(a , b):
+    return a +b
+res=red(32,4)
+print(res)
