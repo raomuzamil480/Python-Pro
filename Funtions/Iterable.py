90824845c1231708d67a5e0d1a600d73d45cc265
raomuzamil480 <raomuzamil480@gmail.com>
2026-07-02 13:45:26 +0500

    Iteable

--- Funtions/Iterable.py
+++ Funtions/Iterable.py
@@ -1,5 +1,5 @@
 # a object can return  its  elements one at a time
-# use in every varables string loop fun dic touple
+# use in every varables string loop fun dic touples
 
 a =[1,2,3,4,5,6]
 for i in a:
