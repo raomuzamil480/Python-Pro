69c432a2e1dbd5f963869546f61e9353ee603887
raomuzamil480 <raomuzamil480@gmail.com>
2026-07-04 00:34:14 +0500

    File_handling

--- OOP/File_handling.py
+++ OOP/File_handling.py
@@ -1,15 +1,39 @@
 # file dedection
-import os
+# import os
+#
+# file = "file.txt"
+#
+# if os.path.exists(file):
+#     print('Yes file exist :')
+#     if os.path.isfile(file):
+#         print('Is file')
+#     elif os.path.isdir(file):
+#         print('Is dir')
+#
+# else:
+#
+#     print("Noooo")
 
-file = "file.txt"
+#========================Write File
 
-if os.path.exists(file):
-    print('Yes file exist :')
-    if os.path.isfile(file):
-        print('Is file')
-    elif os.path.isdir(file):
-        print('Is dir')
+file_txt='i live lahore pakistan'
+file_name='i.txt'
+with open(file=file_name,mode='w') as file:
+    file.write(file_txt)
+    print(f'Created file {file_name}')
+#=========================================JSON
+import json
+file_json={
+    "name":"Rao",
+    "age":"21"
+}
+fi_path='j.json'
+try:
+    with open(file=fi_path,mode='w') as file:
+        json.dump(file_json,file,indent=5)
+        print(f"create succesful {file_json}")
+except FileExistsError:
+    print("Already exist:")
 
-else:
+#===================================TASK to Muzamil creta a .csv file like import csv
 
-    print("Noooo")
